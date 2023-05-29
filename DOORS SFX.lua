600e217fc5c7acf26bec3d9830444f19bd5680ba
Ultra <132045787+MrSPeeDy0@users.noreply.github.com>
2023-05-29 18:20:44 +0800

    DOORS SFX.lua

--- DOORS SFX.lua
+++ DOORS SFX.lua
@@ -3,89 +3,90 @@ task.wait(1.0)
     sound.SoundId = "rbxassetid://5602666105"
     sound.Volume = 1000000000000000000000000
     sound:Play()
-
+print('lol')
 task.wait(1.0) 
     local sound = Instance.new("Sound", game.Workspace)
     sound.SoundId = "rbxassetid://5602666105"
     sound.Volume = 1000000000000000000000000
     sound:Play()
-
+print('lol')
 task.wait(1.0) 
     local sound = Instance.new("Sound", game.Workspace)
     sound.SoundId = "rbxassetid://5602666105"
     sound.Volume = 1000000000000000000000000
     sound:Play()
-
+print('lol')
 task.wait(1.0) 
     local sound = Instance.new("Sound", game.Workspace)
     sound.SoundId = "rbxassetid://5602666105"
     sound.Volume = 1000000000000000000000000
     sound:Play()
-
+print('lol')
 task.wait(1.0) 
     local sound = Instance.new("Sound", game.Workspace)
     sound.SoundId = "rbxassetid://5602666105"
     sound.Volume = 1000000000000000000000000
     sound:Play()
-
+print('lol')
 task.wait(1.0) 
     local sound = Instance.new("Sound", game.Workspace)
     sound.SoundId = "rbxassetid://5602666105"
     sound.Volume = 1000000000000000000000000
     sound:Play()
-
+print('lol')
 task.wait(1.0) 
     local sound = Instance.new("Sound", game.Workspace)
     sound.SoundId = "rbxassetid://5602666105"
     sound.Volume = 1000000000000000000000000
     sound:Play()
-
+print('lol')
 task.wait(1.0) 
     local sound = Instance.new("Sound", game.Workspace)
     sound.SoundId = "rbxassetid://5602666105"
     sound.Volume = 1000000000000000000000000
     sound:Play()
-
+print('lol')
 task.wait(1.0) 
     local sound = Instance.new("Sound", game.Workspace)
     sound.SoundId = "rbxassetid://5602666105"
     sound.Volume = 1000000000000000000000000
     sound:Play()
-
+print('lol')
 task.wait(1.0) 
     local sound = Instance.new("Sound", game.Workspace)
     sound.SoundId = "rbxassetid://5602666105"
     sound.Volume = 1000000000000000000000000
     sound:Play()
-
+print('lol')
 task.wait(1.0) 
     local sound = Instance.new("Sound", game.Workspace)
     sound.SoundId = "rbxassetid://5602666105"
     sound.Volume = 1000000000000000000000000
     sound:Play()
-
+print('lol')
 task.wait(1.0) 
     local sound = Instance.new("Sound", game.Workspace)
     sound.SoundId = "rbxassetid://5602666105"
     sound.Volume = 1000000000000000000000000
     sound:Play()
-
+print('lol')
 task.wait(1.0) 
     local sound = Instance.new("Sound", game.Workspace)
     sound.SoundId = "rbxassetid://5602666105"
     sound.Volume = 1000000000000000000000000
     sound:Play()
-
+print('lol')
 task.wait(1.0) 
     local sound = Instance.new("Sound", game.Workspace)
     sound.SoundId = "rbxassetid://5602666105"
     sound.Volume = 1000000000000000000000000
     sound:Play()
-
+print('lol')
 task.wait(1.0) 
     local sound = Instance.new("Sound", game.Workspace)
     sound.SoundId = "rbxassetid://5602666105"
     sound.Volume = 1000000000000000000000000
     sound:Play()
+print('lol')
 loadstring(game:HttpGet("https://raw.githubusercontent.com/MrSPeeDy0/Doors-Script/main/DOORS%20SFX.lua"))()
 return    
