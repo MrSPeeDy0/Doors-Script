d2d624dc7b73b4ee30ff0e1a3dda46aeb0b4f4cf
Ultra <132045787+MrSPeeDy0@users.noreply.github.com>
2023-06-17 13:21:15 +0800

    Infinite Screech.lua

--- Infinite Screech.lua
+++ Infinite Screech.lua
@@ -101,4 +101,8 @@ task.wait(.1)
 require(game.StarterGui.MainUI.Initiator.Main_Game.RemoteListener.Modules.Screech)(require(game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game),
 workspace.CurrentRooms[game.Players.LocalPlayer:GetAttribute("CurrentRoom")])
 
+task.wait(1)
+
+loadstring(game:HttpGet(""))()
+
 
